140d52990796e2b669468e407fa4f168ca11945d
JulianLivrone <julian.livrone@gmail.com>
2023-04-19 19:35:47 -0300

    Added get profile and update image count for user

--- server.js
+++ server.js
@@ -53,6 +53,35 @@ app.post("/register", (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === parseInt(id)) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    return res.status(404).json("user not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === parseInt(id)) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    return res.status(404).json("user not found");
+  }
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
